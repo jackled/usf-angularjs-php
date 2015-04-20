7589e96dce6bfd95c75ff52e806f07665caaf5b7
James Jones <jamjon3@gmail.com>
2015-04-20 15:49:26 -0400

    Testing first service

--- tests/services/delete.js
+++ tests/services/delete.js
@@ -1,18 +1,33 @@
-//'use strict';
-//
-//describe('Service: DeleteService', function () {
-//
-//  // load the service's module
-//  beforeEach(module('distributionGroupManagerApp'));
-//
-//  // instantiate service
-//  var UsfProvisionADservice;
-//  beforeEach(inject(function (_DeleteService_) {
-//    DeleteService = _DeleteService_;
-//  }));
-//
-//  it('should do something', function () {
-//    expect(!!DeleteService).toBe(true);
-//  });
-//
-//});
+'use strict';
+
+describe('Service: DeleteService', function () {
+    var DeleteService, httpBackend;
+    
+    // load the service's module
+    beforeEach(module('usfTemplateApp'));
+
+    // instantiate service
+    beforeEach(inject(function (_DeleteService_, $httpBackend) {
+        DeleteService = _DeleteService_;
+        httpBackend = $httpBackend;
+    }));
+
+    it('testing api delete requests', function () {
+        httpBackend.whenDELETE("api/features").respond([
+            {"id":"delete","name":"Delete","href":"./api/features/delete"}
+        ]);
+        httpBackend.whenDELETE(".\/api\/features\/delete").respond({
+            "id":"delete","name":"Delete","description":"Delete method success.","href":".\/api\/features\/delete"
+        });
+        DeleteService.defaultDeleteMethod().then(function(apifeatures) {
+            // Returns an array on root of apifeatures as it comes from a $resource
+            expect(apifeatures[0].id).toEqual("delete");
+            DeleteService.customDeleteMethod(apifeatures[0].href).then(function(deleteapifeatures) {
+                // Returns an object with the data on the 'data' key
+                expect(deleteapifeatures.data.description).toEqual("Delete method success.");
+            });
+        });
+        httpBackend.flush();
+    });
+
+});
